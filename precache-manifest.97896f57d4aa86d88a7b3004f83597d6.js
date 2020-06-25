5f88a7005fafd457d95db78d2b61db20ba371071
Jesús Hernández <jesus.hernandezmoreno@outlook.com>
2020-06-25 17:21:25 -0500

    Updates

---
+++ precache-manifest.97896f57d4aa86d88a7b3004f83597d6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b91a81bf550b0f740dc6905b40d448",
+    "url": "/soft-ui-music-player/index.html"
+  },
+  {
+    "revision": "28da15751d8f62ca4c4f",
+    "url": "/soft-ui-music-player/static/css/main.cfef40ea.chunk.css"
+  },
+  {
+    "revision": "dae89443c3b2af56e11a",
+    "url": "/soft-ui-music-player/static/js/2.4e854605.chunk.js"
+  },
+  {
+    "revision": "7f2d1074c2da622aa47ff27e07b02130",
+    "url": "/soft-ui-music-player/static/js/2.4e854605.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28da15751d8f62ca4c4f",
+    "url": "/soft-ui-music-player/static/js/main.5d3d249d.chunk.js"
+  },
+  {
+    "revision": "408519813b6de080831a",
+    "url": "/soft-ui-music-player/static/js/runtime-main.ac4bf20a.js"
+  },
+  {
+    "revision": "e47db5635748bd41ae9fb09690cd5541",
+    "url": "/soft-ui-music-player/static/media/cover.e47db563.jpg"
+  },
+  {
+    "revision": "dcd8cc2cf63e51d92fdf15b0f99fe614",
+    "url": "/soft-ui-music-player/static/media/iphone_11.dcd8cc2c.png"
+  },
+  {
+    "revision": "ab3d0ed6be034a31d6f14db64e0dcdff",
+    "url": "/soft-ui-music-player/static/media/song.ab3d0ed6.mp3"
+  }
+]);
